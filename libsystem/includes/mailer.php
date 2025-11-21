6a5d893727c10ac14ceaa377247d18f7e9deb7ed
secre159 <axlchanh159@gmail.com>
2025-11-22 00:27:58 +0800

    Fix vendor autoload path - should be 2 levels up not 3

--- libsystem/includes/mailer.php
+++ libsystem/includes/mailer.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require __DIR__ . '/../../../vendor/autoload.php';
+require __DIR__ . '/../../vendor/autoload.php';
 
 
 function sendContactMail($name, $email, $message) {
