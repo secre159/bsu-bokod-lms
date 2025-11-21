0335469c5cd7be5b2e54bb7e3c23ffc176ee7b7b
secre159 <axlchanh159@gmail.com>
2025-11-21 10:51:25 +0800

    Fix broken jQuery script tags preventing AJAX from working

--- libsystem/admin/includes/scripts.php
+++ libsystem/admin/includes/scripts.php
@@ -25,9 +25,6 @@
 <!-- AdminLTE App -->
 <script src="../dist/js/adminlte.min.js"></script>
 
-<script src="plugins/jquery/jquery.min.js"></script>
-<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-
 <!-- Active Script -->
 <script>
 $(function(){
